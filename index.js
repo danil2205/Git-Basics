79d9313be88c25351256c28dacb16d1c15fcd2c9
danil2205 <04danil@gmail.com>
2023-02-21 17:16:20 +0200

    added interactive mode
    In interactive mode, the application starts when no arguments have been passed to it. In this mode, the application asks to enter 3 coefficients in turn: a, b and c, displays the resulting equation on the screen and the found roots

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 'use strict';
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const quadraticEquationSolver = (a, b, c) => {
   console.log(`Equation is: ${a} x^2 + ${b} x + ${c} = 0`)
-  if (!a) return `Error. Expected a valid real number, got ${a} instead`;
   const discriminant = b**2 - 4*a*c;
-  if (discriminant < 0) return 'There are 0 roots';
+  if (discriminant < 0) return console.log('There are 0 roots');
   if (discriminant === 0) {
     const x1 = -b / (2*a);
     console.log('There is 1 root');
-    return `x1 = ${x1}`;
+    console.log(`x1 = ${x1}`);
   }
   if (discriminant > 0) {
     const x1 = (-b + Math.sqrt(discriminant)) / (2*a);
     const x2 = (-b - Math.sqrt(discriminant)) / (2*a);
     console.log('There are 2 roots');
-    return `x1 = ${x1}\nx2 = ${x2}`;
+    console.log(`x1 = ${x1}\nx2 = ${x2}`);
   }
 };
 
-console.log(quadraticEquationSolver(2, 1, -3));
 
+const rlQuestion = () => {
+  rl.question('a = ', (a) => {
+    if (/^0x[a-fA-F0-9]+$/.test(a) || Number(a) === 0 || isNaN(Number(a))) {
+      console.log(`Error. Expected a valid real number, got ${a} instead`)
+      rlQuestion();
+    }
+    rl.question('b = ', (b) => {
+      rl.question('c = ', (c) => {
+        quadraticEquationSolver(a, b, c);
+        rl.close();
+      });
+    });
+  });
+};
+
+rlQuestion();
